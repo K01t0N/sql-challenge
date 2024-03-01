1722268483a3d4ade564cbf7fc75a58c1bd278f4
K01t0N <Kolton101@gmail.com>
2024-02-29 21:50:03 -0600

    Update to analysis and ERD

--- EmployeeSQL/analysis.sql
+++ EmployeeSQL/analysis.sql
@@ -1,9 +1,12 @@
 -- List the employee number, last name, first name, sex, and salary of each employee
-SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary FROM employees AS e JOIN salaries AS s ON e.emp_no = s.emp_no
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+    FROM employees AS e 
+	    JOIN salaries AS s ON e.emp_no = s.emp_no
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986
-SELECT first_name, last_name, hire_date FROM employees WHERE hire_date = 1986
--- check this
+SELECT first_name, last_name, hire_date 
+    FROM employees 
+	WHERE EXTRACT(YEAR FROM hire_date) = 1986 -- https://www.sqltutorial.org/sql-date-functions/how-to-extract-year-from-date-in-sql/
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name
 SELECT m.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name
@@ -18,8 +21,10 @@ SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 		JOIN departments AS d ON de.dept_no = d.dept_no
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B (2 points)
-SELECT first_name, last_name, sex FROM employees WHERE first_name = 'Hercules' AND last_name[0] = 'B'
--- check this
+SELECT first_name, last_name, sex 
+    FROM employees 
+    WHERE (first_name = 'Hercules') AND (LEFT(last_name, 1) = 'B')
+	-- https://stackoverflow.com/questions/792294/how-to-get-first-character-of-a-string-in-sql
 
 -- List each employee in the Sales department, including their employee number, last name, and first name (2 points)
 SELECT e.emp_no, e.last_name, e.first_name
@@ -29,5 +34,14 @@ SELECT e.emp_no, e.last_name, e.first_name
 	WHERE d.dept_name = 'Sales'
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name (4 points)
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+    FROM employees AS e 
+	    JOIN dept_emp AS de ON e.emp_no = de.emp_no
+	    JOIN departments AS d ON de.dept_no = d.dept_no
+	WHERE (d.dept_name = 'Sales') OR (d.dept_name = 'Development')
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name) (4 points)
+SELECT last_name, COUNT(last_name)
+    FROM employees
+        GROUP BY last_name
+		ORDER BY last_name DESC
--- EmployeeSQL/erd.md
+++ EmployeeSQL/erd.md
@@ -1,3 +1,23 @@
+titles
+-
+title_id varchar PK
+title
+
+employees
+-
+emp_no integer PK
+emp_title_id varchar FK >- titles.title_id
+birth_date date
+first_name varchar
+last_name varchar
+sex varchar
+hire_date date
+
+salaries
+-
+emp_no integer PK FK - employees.emp_no
+salary integer
+
 departments
 -
 dept_no varchar PK
@@ -5,30 +25,12 @@ dept_name varchar
 
 dept_emp
 -
-emp_no integer PK FK >- employees.emp_no
-dept_no varchar FK >- departments.dept_no
+id serial PK
+emp_no integer FK >- employees.emp_no
+dept_no var_char FK >- departments.dept_no
 
 dept_manager
 -
+id serial PK
 dept_no varchar FK >- departments.dept_no
-emp_no integer PK FK >- employees.emp_no
-
-employees
--
-emp_no integer PK
-emp_title_id varchar FK >- titles.title_id
-birth_date
-first_name varchar
-last_name varchar
-sex varchar
-hire_date
-
-salaries
--
-emp_no integer PK FK >- employees.emp_no
-salary integer
-
-titles
--
-title_id varchar PK
-title varchar
+emp_no integer FK >- employees.emp_no
